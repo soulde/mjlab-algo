6f11b11329fdca6bec7fdad09be6cc9daab9c733
soulde <fakesoulde@gmail.com>
2026-07-06 15:22:22 +0800

    Patch TD-MPC2 compile TF32 warning

--- tests/test_tdmpc2_compile.py
+++ tests/test_tdmpc2_compile.py
@@ -1,5 +1,6 @@
 import importlib
 
+import torch._inductor.compile_fx as compile_fx
 import torch._inductor.config as inductor_config
 
 import mjlab_algo.tdmpc2 as tdmpc2
@@ -16,6 +17,17 @@ def test_configure_tdmpc2_compile_disables_shape_padding_when_enabled():
         assert inductor_config.shape_padding is False
     finally:
         inductor_config.shape_padding = previous
+
+
+def test_configure_tdmpc2_compile_disables_legacy_tf32_warning():
+    previous = compile_fx._warn_tf32_disabled
+    try:
+        configure_tdmpc2_compile(enabled=True)
+
+        assert compile_fx._warn_tf32_disabled is not previous
+        assert compile_fx._warn_tf32_disabled() is None
+    finally:
+        compile_fx._warn_tf32_disabled = previous
 
 
 def test_configure_tdmpc2_compile_leaves_shape_padding_when_disabled():
