479bd5e35e85627c1b3c39a0a55c9fc3a3449e2b
soulde <fakesoulde@gmail.com>
2026-07-06 10:36:09 +0800

    Configure TD-MPC2 compile on package import

--- tests/test_tdmpc2_compile.py
+++ tests/test_tdmpc2_compile.py
@@ -1,5 +1,8 @@
+import importlib
+
 import torch._inductor.config as inductor_config
 
+import mjlab_algo.tdmpc2 as tdmpc2
 from mjlab_algo.tdmpc2.compile import configure_tdmpc2_compile
 
 
@@ -23,5 +26,17 @@ def test_configure_tdmpc2_compile_leaves_shape_padding_when_disabled():
         configure_tdmpc2_compile(enabled=False)
 
         assert inductor_config.shape_padding is True
+    finally:
+        inductor_config.shape_padding = previous
+
+
+def test_tdmpc2_package_import_configures_inductor_before_agent_compile():
+    previous = inductor_config.shape_padding
+    try:
+        inductor_config.shape_padding = True
+
+        importlib.reload(tdmpc2)
+
+        assert inductor_config.shape_padding is False
     finally:
         inductor_config.shape_padding = previous
